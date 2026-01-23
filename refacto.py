84da69b2aad1a1f1a88306b13c6a0f493ac90976
brebelo <brandonrebelopro@gmail.com>
2026-01-23 21:23:25 +0100

    structure finale, commencement des ascii

--- refacto.py
+++
@@ -1,3 +0,0 @@
-liste = []
-#faire une liste pour indexé chaque ascii a un index, il suffira alors de comparer
-#l'idee serait de transformer illumination en index de la liste ce qui est beaucoup plus rapide.
